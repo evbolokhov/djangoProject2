805fa23115b1c23cb12d75d6ecaaf07be8664e46
BEU <evbolohov@yandex.ru>
2022-06-04 13:27:34 +0300

    init

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,14 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+
+def _get_next_day():
+
+    next_day_datetime = timezone.now() + timedelta(days=1) + timedelta(days=1)
+    next_day_datetime.replace(microsecond=0)
+    return  next_day_datetime
 
 
 class TodoTasks(models.Model):
@@ -22,7 +28,7 @@ class TodoTasks(models.Model):
     isImportant = models.BooleanField(default=False, verbose_name="Важная")
     isPublic = models.BooleanField(default=False, verbose_name="Публичная")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    resolutionDate = models.DateTimeField(default=(datetime.now() + timedelta(days=1)), verbose_name="Дата выполнения")
+    resolutionDate = models.DateTimeField(default=_get_next_day, verbose_name="Дата выполнения")
 
     def __str__(self):
         return f"Запись №{self.id}"
